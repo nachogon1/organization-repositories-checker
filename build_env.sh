38d64c29cd1a39b3713c070769d34ebe7bc7f2b8
Ignacio Betegón <i.betegon@going-public.edu>
2021-07-14 21:02:51 +0200

    Add comment to build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -1,2 +1,4 @@
+# This file is not in used.
+# I would use something like this to create the .env files in the pipelines.
 echo GITHUB_TOKEN="WRITE_YOUR_TOKEN_HERE" >> develop.env
 echo GITHUB_TOKEN="WRITE_ORGANIZATION_NAME_HERE" >> develop.env
